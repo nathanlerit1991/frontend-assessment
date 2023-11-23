13badb00119bd3ec86ba8e13ab4ca0fd77adcc7c
nathanlerit1991 <nathan@axiomdata.io>
2023-11-23 15:41:40 +0800

    update UI

---
+++ src/components/Pods.vue
@@ -0,0 +1,24 @@
+<template>
+  <figure class="pods-wrapper text-center p-4">
+    <img src="https://via.placeholder.com/400x300" alt="pod image"/>
+    <figcaption class="my-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet dictum risus eu mollis. Aliquam tempor quam id ex imperdiet, vitae mattis metus malesuada.</figcaption>
+
+    <button class="btn text-black text-uppercase">Read More</button>
+  </figure>
+</template>
+
+<style scoped>
+  .pods-wrapper {
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+  }
+  .pods-wrapper img {
+    width: 100%;
+    border-radius: 8px;
+  }
+  .pods-wrapper button {
+    border-radius: 20px;
+    border: solid 2px #000;
+    font-weight: 700;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<script setup>
+  import Pods from '../components/Pods.vue'
+</script>
+
+<template>
+  <main>
+    <section id="hero" class="mh-100">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col text-center">
+            <h1 class="text-white display-4">Hello Developer!</h1>
+            <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="pods">
+      <div class="container">
+        <div class="row">
+          <div class="cols col-md-4">
+            <Pods/>
+          </div>
+          <div class="cols col-md-4">
+            <Pods/>
+          </div>
+          <div class="cols col-md-4">
+            <Pods/>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
