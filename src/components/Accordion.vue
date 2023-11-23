0632425219b4800332c7247f9e772a3243aa19e1
nathanlerit1991 <nathan@axiomdata.io>
2023-11-23 17:37:48 +0800

    add pods and accordion

---
+++ src/components/Accordion.vue
@@ -0,0 +1,50 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const props = defineProps([
+    'accordionData',
+  ])
+
+  const activeAccordion = ref(0)
+  const toggleAccordion = (index) => {
+    if (activeAccordion.value === index) {
+      // If the same accordion is clicked, close it
+      activeAccordion.value = -1
+    } else {
+      // Open the clicked accordion
+      activeAccordion.value = index
+    }
+  }
+</script>
+<template>
+  <div class="accordion" id="accordionExample">
+    <div 
+      v-for="(accordion, accordion_index) in accordionData" 
+      :key="accordion_index" 
+      class="accordion-item">
+      <h2 class="accordion-header" :id="`heading${accordion_index}`">
+        <button 
+          class="accordion-button"
+          :class="{ collapsed: activeAccordion !== accordion_index }"
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#collapseOne" 
+          aria-expanded="true" 
+          aria-controls="collapseOne"
+          @click="toggleAccordion(accordion_index)"
+        >
+          {{ accordion.title }}
+        </button>
+      </h2>
+      <div 
+        :id="`collapse${accordion_index}`" 
+        class="accordion-collapse collapse"
+        :class="{ show: activeAccordion === accordion_index }"
+        :aria-labelledby="'heading' + accordion_index"
+        data-bs-parent="#accordionExample"
+      >
+        <div class="accordion-body" v-html="accordion.content" />
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,29 @@
 <script setup>
-  import { ref } from 'vue';
+  import { ref, onMounted } from 'vue';
 
   import Pods from '../components/Pods.vue'
+  import Accordion from '../components/Accordion.vue'
+
+  //Fetch Data in data.json
+  const jsonData = ref(null)
+  async function fetchData() {
+    try {
+      const response = await fetch('/contents/data.json')
+
+      if (response.ok) {
+        jsonData.value = await response.json()
+      } else {
+        console.error('Failed to fetch JSON data')
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+
+
+  onMounted(() => {
+    fetchData()
+  })
 
   const podsData = ref([
     { img: 'https://via.placeholder.com/400x300', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor inciddunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
@@ -27,10 +49,18 @@
       <div class="container">
         <div class="row">
           <div
-            v-for="(pod, pod_index) in podsData"
+            v-for="(pod, pod_index) in podsData" :key="pod_index"
             class="cols col-md-4">
             <Pods :description="pod.description"/>
           </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="accordion">
+      <div class="container">
+        <div class="row">
+          <Accordion :accordionData="jsonData" />
         </div>
       </div>
     </section>
