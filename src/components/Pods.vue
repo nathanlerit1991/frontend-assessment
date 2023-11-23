9b1a4f3ff050447e59195b7da733cb16bd2ea688
nathanlerit1991 <nathan@axiomdata.io>
2023-11-23 17:02:36 +0800

    fix responsive

--- src/components/Pods.vue
+++ src/components/Pods.vue
@@ -1,8 +1,14 @@
+<script setup>
+  const props = defineProps([
+    'description',
+  ])
+</script>
+
 <template>
-  <div class="py-2 px-1">
-    <figure class="pods-wrapper text-center p-3 p-md-4">
+  <div class="py-2 px-1 h-100">
+    <figure class="pods-wrapper text-center h-100 p-3 p-md-4">
       <img src="https://via.placeholder.com/400x300" alt="pod image"/>
-      <figcaption class="my-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet dictum risus eu mollis. Aliquam tempor quam id ex imperdiet, vitae mattis metus malesuada.</figcaption>
+      <figcaption class="my-4 pb-4">{{ props.description }}</figcaption>
 
       <button class="btn text-black text-uppercase">Read More</button>
     </figure>
@@ -13,6 +19,7 @@
   .pods-wrapper {
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
     border-radius: 8px;
+    position: relative;
   }
   .pods-wrapper img {
     width: 100%;
@@ -22,5 +29,10 @@
     border-radius: 20px;
     border: solid 2px #000;
     font-weight: 700;
+
+    position: absolute;
+    bottom: 25px;
+    left: 50%;
+    transform: translate(-50%, 0);
   }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <script setup>
+  import { ref } from 'vue';
+
   import Pods from '../components/Pods.vue'
+
+  const podsData = ref([
+    { img: 'https://via.placeholder.com/400x300', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor inciddunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
+    { img: 'https://via.placeholder.com/400x300', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor inciddunt ut labore et dolore magna aliqua.' },
+    { img: 'https://via.placeholder.com/400x300', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor inciddunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco' },
+  ]);
 </script>
 
 <template>
@@ -18,14 +26,10 @@
     <section id="pods" class="py-4 p-md-5 px-2">
       <div class="container">
         <div class="row">
-          <div class="cols col-md-4">
-            <Pods/>
-          </div>
-          <div class="cols col-md-4">
-            <Pods/>
-          </div>
-          <div class="cols col-md-4">
-            <Pods/>
+          <div
+            v-for="(pod, pod_index) in podsData"
+            class="cols col-md-4">
+            <Pods :description="pod.description"/>
           </div>
         </div>
       </div>
